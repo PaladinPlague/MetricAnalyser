507ba2b591297bcdcfa1c42d8ca17d71a35a0d1a
callum <callum.turnbull.2019@uni.strath.ac.uk>
2022-10-14 18:55:33 +0100

    Delete LCOMMap (not necessary anymore)

--- src/ILCOMMap.java
+++
@@ -1,8 +0,0 @@
-import com.github.javaparser.ast.body.MethodDeclaration;
-import com.github.javaparser.ast.expr.NameExpr;
-
-import java.util.Map;
-import java.util.Set;
-
-public interface ILCOMMap extends Map<MethodDeclaration, Set<VariableUse>> {
-}
