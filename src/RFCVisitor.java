3ef22cf9d24b3e3ef76f4467ca61640132b05c0c
Mark McCafferty CS2019 <mark.mccafferty.2019@uni.strath.ac.uk>
2022-10-15 15:10:43 +0100

    Update RFCVisitor.java

--- src/RFCVisitor.java
+++ src/RFCVisitor.java
@@ -4,19 +4,22 @@ import com.github.javaparser.ast.body.BodyDeclaration;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.github.javaparser.ast.body.FieldDeclaration;
 import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.expr.MethodCallExpr;
 import com.github.javaparser.ast.visitor.GenericVisitorAdapter;
 
 import java.sql.Array;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
-public class RFCVisitor extends GenericVisitorAdapter<Set<String>, List<ClassMetricsResult>> {
+public class RFCVisitor extends MetricVisitor<Set<String>> {
 
     public Set<String> visit(ClassOrInterfaceDeclaration coid, List<ClassMetricsResult> arg) {
         NodeList<BodyDeclaration<?>> allMembers = coid.getMembers();
         NodeList<MethodDeclaration> methodsList = new NodeList<>();
         NodeList<ClassOrInterfaceDeclaration> innerClassesList = new NodeList<>();
+        int rfc = 0;
 
         for(BodyDeclaration<?> bd : allMembers) {
             if(bd.isMethodDeclaration()) {
@@ -28,11 +31,25 @@ public class RFCVisitor extends GenericVisitorAdapter<Set<String>, List<ClassMet
         }
 
         Set<String> result = methodsList.accept(this, arg);
-        int noMethods = result.size();
+        Set<String> called = super.visit(coid, arg);
+        System.out.println(coid.getNameAsString());
+        System.out.println("Method List to check if it appears: " + result + " with a length of " + result.size());
+        System.out.println("Methods that are actually called  : " + called + " with a length of " + called.size());
 
+        Iterator<String> allResults = result.iterator();
+        Iterator<String> allCalls = called.iterator();
 
-        super.visit(coid.getMethods(), arg);
-
+        while(allResults.hasNext()){
+            String outerResult = allResults.next();
+            while(allCalls.hasNext()){
+                String innerCall = allCalls.next();
+                if(outerResult.equals(innerCall)){
+                    rfc++;
+                }
+            }
+            allCalls = called.iterator();
+        }
+        System.out.println(rfc);
         int foundIndex = -1;
         for(int i=0; i<arg.size(); i++) {
             ClassMetricsResult cmr = arg.get(i);
@@ -47,7 +64,7 @@ public class RFCVisitor extends GenericVisitorAdapter<Set<String>, List<ClassMet
             foundIndex = arg.size()-1;
         }
 
-        arg.get(foundIndex).setRfc(0);
+        arg.get(foundIndex).setRfc(rfc + arg.get(foundIndex).getWcm());
 
         return !coid.isInnerClass() ? result : null;
     }
@@ -78,12 +95,25 @@ public class RFCVisitor extends GenericVisitorAdapter<Set<String>, List<ClassMet
         return hasResult ? total : null;
     }
 
-
     @Override
     public Set<String> visit(MethodCallExpr calls, List<ClassMetricsResult> arg) {
         super.visit(calls, arg);
+        Set<String> c = new HashSet<>();
+        c.add(String.valueOf(calls.getName()));
+        return c;
+    }
 
-        return null;
+    protected Set<String> initialiseEmpty() {
+        return new HashSet<>();
+    }
+    protected Set<String> combineResults(Set<String> first, Set<String> second) {
+        Set<String> combined = new HashSet<>();
+        combined.addAll(first);
+        combined.addAll(second);
+        return combined;
+    }
+    protected Set<String> getReturn(Set<String> possibleReturn) {
+        return possibleReturn.size() > 0 ? possibleReturn : null;
     }
 
 }
