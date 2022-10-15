5fb40269b29f7af122495d47f73c7f6ba19450b1
Mark McCafferty CS2019 <mark.mccafferty.2019@uni.strath.ac.uk>
2022-10-15 11:54:03 +0100

    Update Driver.java

--- src/Driver.java
+++ src/Driver.java
@@ -1,75 +1,89 @@
-import com.github.javaparser.StaticJavaParser;
-import com.github.javaparser.ast.CompilationUnit;
-import com.github.javaparser.ast.visitor.GenericVisitor;
+import com.github.javaparser.ast.Node;
+import com.github.javaparser.ast.NodeList;
+import com.github.javaparser.ast.body.BodyDeclaration;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.FieldDeclaration;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.visitor.GenericVisitorAdapter;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileWriter;
-import java.util.ArrayList;
+import java.sql.Array;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-public class Driver {
-    private static final String DIRECTORY = "CS409TestSystem2022";
+public class RFCVisitor extends GenericVisitorAdapter<Set<String>, List<ClassMetricsResult>> {
 
-    public static void main(String[] args) {
-        File directory = new File(DIRECTORY);
+    public Set<String> visit(ClassOrInterfaceDeclaration coid, List<ClassMetricsResult> arg) {
+        NodeList<BodyDeclaration<?>> allMembers = coid.getMembers();
+        NodeList<MethodDeclaration> methodsList = new NodeList<>();
+        NodeList<ClassOrInterfaceDeclaration> innerClassesList = new NodeList<>();
 
-        analyseDirectory(directory);
-    }
-
-    public static void analyseDirectory(File directory) {
-        GenericVisitor<Integer, List<ClassMetricsResult>> WMCVisitor = new WMCVisitor();
-        GenericVisitor<Set<String>, List<ClassMetricsResult>> RFCVisitor = new RFCVisitor();
-        File outputFile = new File("output.csv");
-
-        try(FileWriter output = new FileWriter(outputFile)) {
-
-
-            for(File subDirectory : directory.listFiles()) {
-                writeAndPrint(output, subDirectory.getName()+"\n");
-                writeAndPrint(output, "Class Name,WCM,RFC,CBO,LCOM\n");
-
-                List<ClassMetricsResult> resultMap = new ArrayList<>();
-
-                for(File f : subDirectory.listFiles()) {
-
-
-                    String extension = "";
-                    String fileName = f.getName();
-
-                    int i = fileName.lastIndexOf('.');
-                    if (i > 0) {
-                        extension = fileName.substring(i+1);
-                    }
-
-                    if(extension.equals("java")) {
-                        CompilationUnit cu = StaticJavaParser.parse(new FileInputStream(f));
-
-                        WMCVisitor.visit(cu, resultMap);
-                        RFCVisitor.visit(cu, resultMap);
-                    }
-
-
-                }
-                for(ClassMetricsResult cmr : resultMap) {
-                    writeAndPrint(output, cmr.toCSVString());
-                }
-                writeAndPrint(output, "\n");
+        for(BodyDeclaration<?> bd : allMembers) {
+            if(bd.isMethodDeclaration()) {
+                methodsList.add(bd.asMethodDeclaration());
+            } else if(bd.isClassOrInterfaceDeclaration()) {
+                innerClassesList.add(bd.asClassOrInterfaceDeclaration());
             }
-        } catch (Exception e) {
-            System.out.println("Failed to read directory");
-        }
-    }
-
-    private static void writeAndPrint(FileWriter output, String line) {
-        try {
-            output.write(line);
-            System.out.print(line);
-        } catch (Exception ignored) {
 
         }
 
+        Set<String> result = methodsList.accept(this, arg);
+        int noMethods = result.size();
 
+
+        super.visit(coid.getMethods(), arg);
+
+        int foundIndex = -1;
+        for(int i=0; i<arg.size(); i++) {
+            ClassMetricsResult cmr = arg.get(i);
+            if(cmr.getCoid().equals(coid)) {
+                foundIndex = i;
+                break;
+            }
+        }
+
+        if(foundIndex == -1) {
+            arg.add(new ClassMetricsResult(coid));
+            foundIndex = arg.size()-1;
+        }
+
+        arg.get(foundIndex).setRfc(0);
+
+        return !coid.isInnerClass() ? result : null;
     }
+
+
+
+    @Override
+    public Set<String> visit(MethodDeclaration md, List<ClassMetricsResult> arg) {
+        super.visit(md, arg);
+        Set<String> s = new HashSet<>();
+        s.add(md.getNameAsString());
+        return s;
+    }
+
+    @Override
+    public Set<String> visit(NodeList n, List<ClassMetricsResult> arg) {
+        Set<String> total = new HashSet<>();
+        // n is a list of code
+        boolean hasResult = false;
+
+        for (final Object v : n) {
+            Set<String> result = ((Node) v).accept(this, arg);
+            if (result != null) {
+                total.addAll(result);
+                hasResult = true;
+            }
+        }
+        return hasResult ? total : null;
+    }
+
+
+    @Override
+    public Set<String> visit(MethodCallExpr calls, List<ClassMetricsResult> arg) {
+        super.visit(calls, arg);
+
+        return null;
+    }
+
 }
